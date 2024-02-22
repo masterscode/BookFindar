7491332018b02ad7ff1c60424ac225c98115a76a
Emmanuel Ogbinaka <ogbinakaemmanuel@gmail.com>
2024-02-23 00:35:46 +0100

    Fix errors from manual test

--- src/main/java/com/findar/book/repository/BookRepository.java
+++ src/main/java/com/findar/book/repository/BookRepository.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query("select b from Book b where (:author='' or b.author=:author) AND (:title ='' or b.title = :title) " +
+    @Query("select b from Book b where (:author='' or b.author=:author) AND (:title ='' or b.title like '%'||:title||'%') " +
             " AND (:price=0 or b.price = :price) " +
             " AND ((:start is null or :end is null ) or (date(b.dateCreated) between date(:start) and date(:end)))")
     Page<Book> filterBooks(String author, String title, Double price, LocalDateTime start, LocalDateTime end, Pageable pageable);
